ce20ff9876a117067ef206788cbcc636a646fbf3
Andrei Mihai <andreimihai2305is@gmail.com>
2025-05-11 18:41:56 +0300

    started work on term_freqs

--- main.c
+++ main.c
@@ -1,12 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <dirent.h>
+#include <string.h>
 
+#define M 50
+
+typedef struct { 
+    size_t freq;
+    char* term;
+} Term_Freq;
+
+typedef struct Term_Freqs Term_Freqs;
+struct Term_Freqs {
+   Term_Freq tf;
+   Term_Freqs *next;
+};
+
+int hash(char* term);
 
 int main(int argc, char** argv)
 {
+    if (argc < 2)
+    {
+        fprintf(stderr, "ERROR: No Folder provided!\n");
+        fprintf(stderr, "USAGE: ./main [FOLDER]\n");
+        exit(1);
+    }
+
     struct dirent* entry;
-    DIR* dr = opendir("../");
+    char* dir_name = argv[1];
+    DIR* dr = opendir(dir_name);
 
     if (dr == NULL)
     {
@@ -16,10 +39,30 @@ int main(int argc, char** argv)
     
     while((entry = readdir(dr)) != NULL)
     {
-        printf("%s\n", entry->d_name);
+        if (entry->d_type == 0x08){
+            printf("FOUND TEXT FILE %s\n", entry->d_name);
+            char* s = strcat(dir_name, entry->d_name); 
+            FILE *f = fopen(s, "rb");
+            if (f == NULL) {
+                fprintf(stderr, "ERROR could not open file %s!\n", s);
+            }
+            
+            fclose(f);
+        }
     }
 
     closedir(dr);
-
     return 0;
 }
+
+int hash(char* term)
+{
+    int sum = 0; 
+    for (size_t i = 0; i < strlen(term); ++i)
+    {
+        sum += term[i];
+    }
+    return sum;
+}
+
+
