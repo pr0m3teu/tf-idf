afd1ec2fddf9e1fdd84ab62b0b1a7ab9b85c14bc
Andrei Mihai <andreimihai2305is@gmail.com>
2025-05-11 16:51:15 +0300

    reading folder contents code

--- main.c
+++ main.c
@@ -1,16 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <dirent.h>
 
 
 int main(int argc, char** argv)
 {
-    struct direct entry;
+    struct dirent* entry;
     DIR* dr = opendir("../");
 
     if (dr == NULL)
     {
-
+        fprintf(stderr, "ERROR: could not open folder!\n");
+        exit(1);
     }
+    
+    while((entry = readdir(dr)) != NULL)
+    {
+        printf("%s\n", entry->d_name);
+    }
+
+    closedir(dr);
 
     return 0;
 }
