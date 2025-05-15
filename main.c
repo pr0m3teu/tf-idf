fefd00560ffe04ec399b975393375c0081b1de78
Andrei Mihai <andreimihai2305is@gmail.com>
2025-05-15 15:54:56 +0300

    continued work

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <dirent.h>
 #include <string.h>
 #include <ctype.h>
@@ -20,7 +21,8 @@ typedef struct {
 } Term_Freq;
 
 typedef struct {
-    char*            doc_name;
+    size_t            terms_count;
+    char*             doc_name;
     Term_Freq_List**  term_freqs;
 } Doc;
 
@@ -28,8 +30,11 @@ int hash(char* term);
 void append_term(Term_Freq_List** ht, char* term);
 void print_ht(Term_Freq_List** ht);
 void free_ht(Term_Freq_List** ht);
-
+bool search_ht(Term_Freq_List** ht, char* item);
 void free_doc(Doc* doc);
+
+static Doc* docs[M];
+static size_t docs_len = 0;
 
 int main(int argc, char** argv)
 {
@@ -55,6 +60,7 @@ int main(int argc, char** argv)
         if (entry->d_type == 0x08) {
             Doc *doc = malloc(sizeof(Doc)); 
             doc->term_freqs = malloc(sizeof(Term_Freq_List) * M);
+            doc->terms_count = 0;
             char* doc_name = malloc(sizeof(char) * (strlen(dir_name) + strlen(entry->d_name) + 1)); 
             if (doc_name == NULL)
             {
@@ -94,6 +100,7 @@ int main(int argc, char** argv)
                     for(; *t; ++t) *t = tolower(*t);
 
                     append_term(doc->term_freqs, word);
+                    doc->terms_count++;
                     word = strtok(NULL, SPECIAL_CHARS);
                 }
             }
@@ -104,12 +111,18 @@ int main(int argc, char** argv)
             buff = NULL;
 
             print_ht(doc->term_freqs);
-            free_doc(doc);
-            
+            docs[docs_len++] = doc; 
+            for(size_t i = 0; i < docs_len; ++i) 
+            {   
+                printf("docs[i]->doc_name %s, terms_count %ld\n" , docs[i]->doc_name, docs[i]->terms_count);
+                printf("search for masina: %d\nsearch for scribes: %d\n", search_ht(docs[i]->term_freqs, "masina"), search_ht(docs[i]->term_freqs, "scribes"));
+            }
+
         }
     }
 
     closedir(dr);
+    for (size_t i = 0; i < docs_len; ++i) free_doc(docs[i]);
     return 0;
 }
 
@@ -206,4 +219,19 @@ void free_doc(Doc* doc)
     free(doc->doc_name);
     doc->doc_name = NULL;
     free(doc);
+}
+
+bool search_ht(Term_Freq_List** ht, char* item)
+{
+    int key = hash(item);
+    Term_Freq_List* entry= ht[key];
+
+    if (entry == NULL) return false;
+
+    while(entry != NULL)
+    {
+        if (strcmp(entry->term, item) == 0) return true;
+        entry = entry->next;
+    }
+    return false;
 }
