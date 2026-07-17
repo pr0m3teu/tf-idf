b0a9414cbdc7a1cb9da5a733f8c662552d835847
Andrei Mihai <andreimihai2305is@gmail.com>
2026-07-17 11:40:53 +0300

    began making search interactive

--- main.c
+++ main.c
@@ -1,8 +1,11 @@
 #include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 #include <dirent.h>
 
 #include "tfidf.h"
+
+#define BUF_SIZE 1024
 
 int main(int argc, char** argv)
 {
@@ -16,20 +19,66 @@ int main(int argc, char** argv)
     const char* dir_name = argv[1];
     DIR* dr = opendir(dir_name);
 
-
     if (dr == NULL)
     {
         fprintf(stderr, "ERROR: could not open folder!\n");
         exit(1);
     }
 
-
     da docs = {0};
     da_init(&docs);
 
-    index_documents(dr, &docs, dir_name);
+    printf("Began indexing folder: %s\n", dir_name);
+    if (index_documents(dr, &docs, dir_name) < 0)
+    {
+        exit(1);
+    }
+    printf("Finished!\n");
     
+    double max_score = 0;
+    Doc relevant_docs[10] = {0};
+    int k = 0;
+
+    char buf[BUF_SIZE];
+
+    bool ok = true;
+    while (ok)
+    {
+        printf("Search term > ");
+        fgets(buf, BUF_SIZE, stdin);
+        
+        buf[strlen(buf) - 1] = 0;
+        
+        printf("> Searching for: \"%s\"\n", buf);
+        for (size_t i = 0; i < docs.size; ++i)
+        {
+            double score = tf_idf(&docs, *docs.items[i], buf);
+            if (score > max_score && k < 10)
+            {
+                relevant_docs[k++] = *docs.items[i];
+                max_score = score;
+            }
+        }
+ 
+        
+        printf("> Found %d relevant files:\n", k);
+        for (int i = 0; i < k; ++i)
+        {
+            printf("Doc %d: %s\n", i, relevant_docs[i].doc_name);
+        }
+
+        printf("Continue (yes: y, no: n): ");
+        fgets(buf, BUF_SIZE, stdin);
+
+        buf[strlen(buf) - 1] = 0;
+        if (strcmp(buf, "y") != 0) ok = false;
+        k = 0;
+        max_score = 0;
+
+    }
+       
     closedir(dr);
+   
     for (size_t i = 0; i < docs.size; ++i)
     {
         free_doc(docs.items[i]);
